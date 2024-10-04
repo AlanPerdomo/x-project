883e427c658726dd7e87cdb59f51b6fbbffd335b
Alan Perdomo <alanperdomo@hotmail.com>
2024-10-04 13:12:34 -0300

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const { Client,Events,  GatewayIntentBits, EmbedBuilder } = require('discord.js');
+const { TOKEN } = process.env;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+client.login(TOKEN);
