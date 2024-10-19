d300a7c10930da249c1bcc7f2c385792acca27d3
Alan Perdomo <alanperdomo@hotmail.com>
2024-10-19 20:27:20 -0300

    Update command description and simplify user registration logic.

--- commands/utility/cadastrarUsuario.js
+++ commands/utility/cadastrarUsuario.js
@@ -5,42 +5,30 @@ const { userService } = require('../services/UserService');
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('cadastrarusuario')
-        .setDescription('registra seu usuario no sistema do bot.')
+        .setDescription('Vincula o seu email ao seu usuario do discord no Heimer-bot.')
         .addStringOption(option =>
             option.setName('email')
                 .setDescription('Informe seu email')
                 .setRequired(true)
-        ).addStringOption(option =>
-            option.setName('senha')
-                .setDescription('Informe sua senha')
-                .setRequired(true)
-        ).addStringOption(option => 
-            option.setName('confirmação')
-                .setDescription('Confirme sua senha')
-                .setRequired(true),
         ),
-        async execute(interaction) {
-            if(interaction.options.getString('confirmação') !== interaction.options.getString('senha')) {
-                await interaction.reply('As senhas devem ser iguais!');
-            } else {
-                let data = {
-                    email: interaction.options.getString('email'),
-                    password: interaction.options.getString('senha'),
-                    name: interaction.user.globalName,
-                    discordId: interaction.user.id,
-                    username: interaction.user.username,
-                    type: 'user'
-                }
+    async execute(interaction) {
+        let data = {
+            email: interaction.options.getString('email'),
+            name: interaction.user.globalName,
+            discordId: interaction.user.id || "",
+            username: interaction.user.username,
+            type: 'user'
+        };
 
-                try {
-                    await userService.cadastrar(data);
-                    await interaction.reply('Cadastrado com sucesso!');
-                    console.log('Cadastrado com sucesso!');
-                } catch (error) {
-                    await interaction.reply('Erro ao cadastrar o usuário!');
-                    console.log(error);
-                }
-                console.log(interaction);
-            }
+        console.log(data);
+
+        try {
+            await userService.cadastrar(data).then(result => {
+                return interaction.reply(result.message);
+            });
+        } catch (error) {
+            return interaction.reply(error.message);
         }
+    }
 };
+
